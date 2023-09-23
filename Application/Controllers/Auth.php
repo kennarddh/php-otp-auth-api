5f8d278fc4caaf3ff91b7b099ab2d4f28a82662c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-09-23 17:31:31 +0700

    feat: Redis otp

--- Application/Controllers/Auth.php
+++ Application/Controllers/Auth.php
@@ -5,6 +5,7 @@ namespace Application\Controllers;
 use Internal\Controllers\BaseController;
 use Internal\Database\Database;
 use Internal\Logger\Logger;
+use Redis;
 
 final class Auth extends BaseController
 {
@@ -21,11 +22,27 @@ final class Auth extends BaseController
 		if (!$success) {
 			$this->response->send(["error" => "Internal server error"], 500);
 
-			Logger::log('error', "Insert user error");
+			Logger::log('error', "Insert user db error");
 
 			return;
 		}
 
-		$this->response->send([], 201);
+		$redis = new Redis();
+
+		$successRedisConnect = $redis->connect('127.0.0.1');
+
+		if (!$successRedisConnect) {
+			$this->response->send(["error" => "Internal server error"], 500);
+
+			Logger::log('error', "Insert user redis error");
+
+			return;
+		}
+
+		$otp = random_int(100000, 999999);
+
+		$redis->set($body->username, $otp, 2 * 60);
+
+		$this->response->send(["otp" => $otp], 201);
 	}
 }
